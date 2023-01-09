9116d35fa98d8e8fef412bfb87e2a942608155d1
Idrees Haddad <gelnox@live.com>
2023-01-09 20:17:19 +0300

    Image uploader funtional, stying left

--- angular/src/app/directive/image-uploader/image-uploader.component.ts
+++ angular/src/app/directive/image-uploader/image-uploader.component.ts
@@ -1,4 +1,10 @@
-import { HttpClient, HttpEventType, HttpErrorResponse, HttpResponse, HttpEvent } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpEventType,
+  HttpErrorResponse,
+  HttpResponse,
+  HttpEvent,
+} from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { ImageUploaderConfig } from './image-uploader.config';
@@ -8,27 +14,26 @@ import { UploaderMode } from './uploaderMode.enum';
 @Component({
   selector: 'app-image-uploader',
   templateUrl: './image-uploader.component.html',
-  styleUrls: ['./image-uploader.component.css']
+  styleUrls: ['./image-uploader.component.css'],
 })
 export class ImageUploaderComponent implements OnInit {
-
   progress!: number;
-  imageSrc!: string;
+  silhouetteImage!: string;
   uploaderModeEnum = UploaderMode;
+
+  imagesNames: UploaderImage[] = [];
 
   @Output() public onUploadFinished = new EventEmitter();
 
   @Input() public config!: ImageUploaderConfig;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-
     this.setSilhouetteImage();
   }
 
   uploadFile(files: FileList | null) {
-
     if (files === null) {
       return;
     }
@@ -39,40 +44,44 @@ export class ImageUploaderComponent implements OnInit {
       formData.append('files', files[i]);
     }
 
-    this.http.post(environment.uploadUrl, formData, { reportProgress: true, observe: 'events' })
+    this.http
+      .post(environment.uploadUrl, formData, {
+        reportProgress: true,
+        observe: 'events',
+      })
       .subscribe({
         next: (event) => {
           if (event.type === HttpEventType.UploadProgress) {
             if (event.total == undefined) {
               event.total = 1;
-              alert("event total is undefined");
+              alert('event total is undefined');
             }
-            this.progress = Math.round(100 * event.loaded / event.total);
-          }
-          else if (event.type === HttpEventType.Response) {
-
+            this.progress = Math.round((100 * event.loaded) / event.total);
+          } else if (event.type === HttpEventType.Response) {
             let uploaderImages = event.body as UploaderImage[];
             this.onUploadFinished.emit(uploaderImages);
 
-            this.imageSrc = `${environment.imgStorageUrl}/${uploaderImages[0].name}`;
+            this.imagesNames = uploaderImages;
           }
         },
-        error: (err: HttpErrorResponse) => console.log(err)
+        error: (err: HttpErrorResponse) => console.log(err),
       });
+  }
+
+  getImageUrl(img: UploaderImage) : string {
+
+    return `${environment.imgStorageUrl}/${img.name}`;
   }
 
   //#region Private
 
   private setSilhouetteImage() {
-
     if (this.config.mode == UploaderMode.Normal) {
-      this.imageSrc = '../../../assets/imgs/item.png';
-    }
-    else {
-      this.imageSrc = '../../../assets/imgs/user.png';
+      this.silhouetteImage = '../../../assets/imgs/item.png';
+    } else {
+      this.silhouetteImage = '../../../assets/imgs/user.png';
     }
   }
 
   //#endregion
-
 }
